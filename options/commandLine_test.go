4cc6c4c4c4e9c45b1ed0a1f5487ce98342a5e523
Pieter Germishuys <pieter@pieterg.com>
2018-01-21 10:27:06 +0100

    Add additional tests covering command line parsing

--- options/commandLine_test.go
+++ options/commandLine_test.go
@@ -12,6 +12,15 @@ func TestCommandLine_With_No_Arguments(t *testing.T) {
 	}
 }
 
+func TestCommandLine_With_Single_Invalid_Argument(t *testing.T) {
+	args := []string{"-", "port=8080"}
+	_, err := parseCommandLine(args)
+
+	if err == nil {
+		t.Error("Expected an error, but received none")
+	}
+}
+
 func TestCommandLine_With_Single_Long_Form_Argument(t *testing.T) {
 	args := []string{"--port=8080"}
 	optionSources, err := parseCommandLine(args)
@@ -114,6 +123,36 @@ func TestCommandLine_With_Multiple_Long_Form_Argument_Without_Equals(t *testing.
 	}
 
 	expectedValue = "localhost"
+	if optionSource.Value != expectedValue {
+		t.Errorf("Expected value to be %s, but got %s", expectedValue, optionSource.Value)
+	}
+}
+
+func TestCommandLine_With_Single_Short_Form_Argument(t *testing.T) {
+	args := []string{"--mem-db-"}
+	optionSources, err := parseCommandLine(args)
+
+	if err != nil {
+		t.Errorf("Expected no error, but received %s", err.Error())
+	}
+
+	if len(optionSources) != 1 {
+		t.Errorf("Expected a single option source, but received %+v", len(optionSources))
+	}
+
+	optionSource := optionSources[0]
+
+	expectedSource := "Command Line"
+	if optionSource.Source != expectedSource {
+		t.Errorf("Expected source to be %s, but got %s", expectedSource, optionSource.Source)
+	}
+
+	expectedName := "mem-db"
+	if optionSource.Name != expectedName {
+		t.Errorf("Expected name to be %s, but got %s", expectedName, optionSource.Name)
+	}
+
+	expectedValue := "-"
 	if optionSource.Value != expectedValue {
 		t.Errorf("Expected value to be %s, but got %s", expectedValue, optionSource.Value)
 	}
